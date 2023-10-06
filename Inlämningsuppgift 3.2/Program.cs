2af69e858f4c8908f1ccc604a6948f482d63883e
arha06001 <arha06001@E-PW0234JA>
2023-10-06 12:58:51 +0200

    Add project files.

---
+++ Inlämningsuppgift 3.2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Har du gått ut gymnasiet? (j/n): ");
+        char harGåtUtGymnasiet = Console.ReadKey().KeyChar;
+        Console.WriteLine();
+
+        if (harGåtUtGymnasiet == 'j' || harGåtUtGymnasiet == 'J')
+        {
+            Console.Write("Hur gammal är du? Ange ålder: ");
+            int ålder = int.Parse(Console.ReadLine());
+
+            if (ålder < 22)
+            {
+                Console.WriteLine("Vi vill anställa dig.");
+            }
+            else
+            {
+                Console.WriteLine("Vi kan tyvärr inte anställa dig.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Vi kan tyvärr inte anställa dig.");
+        }
+    }
+}
